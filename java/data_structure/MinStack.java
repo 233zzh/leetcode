422b5c3ae798c10b3ef209afdee94cabedc704b5
harryyzhang <harryyzhang@tencent.com>
2023-05-08 12:43:41 +0800

    小灰算法面试-最小栈

---
+++ java/data_structure/MinStack.java
@@ -0,0 +1,50 @@
+package data_structure;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+/**
+ * @Description 最小栈
+ * @Author 爱做梦的鱼
+ * @Blog https://zihao.blog.csdn.net/
+ * @Date 2023/5/8 11:26
+ */
+public class MinStack {
+
+  private Stack<Integer> mainStack = new Stack<Integer>();
+
+  private Stack<Integer> minStack = new Stack<Integer>();
+
+  public void push(int element) {
+    if (minStack.isEmpty() || element <= minStack.peek()) {
+      minStack.push(element);
+    }
+    mainStack.push(element);
+  }
+
+  public int pop() {
+    if (mainStack.peek().equals(minStack.peek())) {
+      minStack.pop();
+    }
+    return mainStack.pop();
+  }
+
+  public int getMin() {
+    return minStack.peek();
+  }
+
+  public static void main(String[] args) throws Exception {
+    MinStack stack = new MinStack();
+//    stack.push(4);
+//    stack.push(9);
+//    stack.push(7);
+//    stack.push(3);
+//    stack.push(8);
+//    stack.push(5);
+//    System.out.println(stack.getMin());
+//    stack.pop();
+//    stack.pop();
+//    stack.pop();
+    System.out.println(stack.getMin());
+  }
+}
