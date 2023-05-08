9816b778b2ce749c345e9d59a9e5c15b89545d79
harryyzhang <harryyzhang@tencent.com>
2023-05-08 12:45:23 +0800

    小灰算法面试-如何判断一个数是否为2的整数次幂

---
+++ java/data_structure/IsPowerOf2.java
@@ -0,0 +1,14 @@
+package data_structure;
+
+/**
+ * @Description 如何判断一个数是否为2的整数次幂
+ * @Author 爱做梦的鱼
+ * @Blog https://zihao.blog.csdn.net/
+ * @Date 2023/5/8 12:44
+ */
+public class IsPowerOf2 {
+
+  public static boolean isPowerOf2(int num) {
+    return (num & num - 1) == 0;
+  }
+}
