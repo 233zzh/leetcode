95bc5c122d792fa5e22c9cd64e07191f99c6d1ea
harryyzhang <harryyzhang@tencent.com>
2023-05-08 16:30:03 +0800

    小灰漫画算法-如何判断一个数是否为2的整数次幂

--- java/xiaohui_algorithm/interview/IsPowerOf2.java
+++ java/xiaohui_algorithm/interview/IsPowerOf2.java
@@ -1,7 +1,7 @@
 package xiaohui_algorithm.interview;
 
 /**
- * @Description 如何判断一个数是否为2的整数次幂
+ * @Description 小灰漫画算法-如何判断一个数是否为2的整数次幂
  * @Author 爱做梦的鱼
  * @Blog https://zihao.blog.csdn.net/
  * @Date 2023/5/8 12:44
@@ -10,5 +10,10 @@ public class IsPowerOf2 {
 
   public static boolean isPowerOf2(int num) {
     return (num & num - 1) == 0;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(isPowerOf2(16));
+    System.out.println(isPowerOf2(10));
   }
 }
